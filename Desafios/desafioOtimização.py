4230003abd8825487dd075736a9c656257cd7963
CaioSViana <47906580+CaioSViana@users.noreply.github.com>
2026-01-02 23:36:44 -0300

    Código atualizado com as duas funções

--- Desafios/desafioOtimização.py
+++ Desafios/desafioOtimização.py
@@ -3,6 +3,8 @@ menu = """
 [d] Depositar
 [s] Sacar
 [e] Extrato
+[u] Criar Usuario
+[c] Criar Conta
 [q] Sair
 
 => """
@@ -12,8 +14,12 @@ limite = 500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
+usuarios = []
+contas = []
+AGENCIA = "0001"
 
-def depositar(extrato, valor, /):
+
+def depositar(saldo, valor, extrato, /):
     
     if valor > 0:
         saldo += valor
@@ -21,13 +27,13 @@ def depositar(extrato, valor, /):
 
     else:
         print("Operação falhou! O valor informado é inválido.")
+    
+    return saldo, extrato
 
-def sacar(*,valor = valor, saldo = saldo, extrato = extrato): 
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques): 
 
     excedeu_saldo = valor > saldo
-
     excedeu_limite = valor > limite
-
     excedeu_saques = numero_saques >= LIMITE_SAQUES
 
     if excedeu_saldo:
@@ -46,29 +52,89 @@ def sacar(*,valor = valor, saldo = saldo, extrato = extrato):
 
     else:
         print("Operação falhou! O valor informado é inválido.")
+    
+    return saldo, extrato, numero_saques
 
-def extrato(saldo, /, *, extrato):
+def exibir_extrato(saldo, /, *, extrato):
     print("\n================ EXTRATO ================")
     print("Não foram realizadas movimentações." if not extrato else extrato)
     print(f"\nSaldo: R$ {saldo:.2f}")
     print("==========================================")
 
-def 
+def criar_usuario(nome, data_nasc, cpf, endereco):
+    cpf_limpo = cpf.replace(".", "").replace("-", "")
+
+    for usuario in usuarios:
+        if usuario["cpf"] == cpf_limpo:
+            print("Erro: este usuário já existe")
+            return
+
+    novo_usuario = {
+        "nome": nome,
+        "data_nasc": data_nasc,
+        "cpf": cpf_limpo,
+        "endereco": endereco
+    }
+    
+    usuarios.append(novo_usuario)
+    print("Usuário cadastrado com sucesso!")
+
+def criar_conta(cpf):
+    cpf_limpo = cpf.replace(".", "").replace("-", "")
+    
+    usuario_encontrado = None
+    for usuario in usuarios:
+        if usuario["cpf"] == cpf_limpo:
+            usuario_encontrado = usuario
+            break
+    
+    if not usuario_encontrado:
+        print("\nErro: Usuário não encontrado! Cadastre o usuário antes de criar uma conta.")
+        return
+
+    numero_conta = len(contas) + 1
+    
+    nova_conta = {
+        "agencia": AGENCIA,
+        "numero_conta": numero_conta,
+        "usuario": usuario_encontrado
+    }
+    
+    contas.append(nova_conta)
+    print(f"\nConta {numero_conta} criada com sucesso")
 
 while True:
 
     opcao = input(menu)
 
     if opcao == "d":
-        depositar(extrato, valor)
-        valor = float(input("Informe o valor do saque: "))
+        valor = float(input("Informe o valor do depósito: "))
+        saldo, extrato = depositar(saldo, valor, extrato)
 
     elif opcao == "s":
-        sacar(valor = valor, saldo = saldo,extrato = extrato)
-        valor = float(input("Informe o valor do depósito: "))
+        valor = float(input("Informe o valor do saque: "))
+        saldo, extrato, numero_saques = sacar(
+                saldo=saldo,
+                valor=valor,
+                extrato=extrato,
+                limite=limite,
+                numero_saques=numero_saques,
+                limite_saques=LIMITE_SAQUES,
+            )
 
     elif opcao == "e":
-        extrato(saldo, extrato = extrato)
+        exibir_extrato(saldo, extrato = extrato)
+
+    elif opcao == "u":
+        nome = input("Informe o nome completo: ")
+        data_nasc = input("Informe a data de nascimento (ex: 21/01/2000): ")
+        cpf = input("Informe o CPF: ")
+        endereco = input("Informe o endereço completo (Rua, Número, Bairro, Cidade/UF): ")
+        criar_usuario(nome, data_nasc, cpf, endereco)
+
+    elif opcao == "c":
+        cpf = input("Informe o CPF do titular: ")
+        criar_conta(cpf)
 
     elif opcao == "q":
         break
